615ca56f47d5f8aef8cdb50850772fd457d89e31
king17pvp <tuanhai1304@gmail.com>
2025-06-02 22:47:32 +0700

    Added read config function

--- include/utils.h
+++ include/utils.h
@@ -9,4 +9,6 @@ void add_conv_layer(CNN *cnn, int out_channels, int kernel_size, int in_channels
 void add_fc_layer(CNN *cnn, int in_features, int out_features, float mean, float stddev);
 void save_cnn_weights(CNN *cnn, const char *filename);
 void load_cnn_weights(CNN *cnn, const char *filename);
+void load_config_from_txt(const char *filename, int *input_w, int *input_h, int *input_c,
+                          int *num_conv, int *ks, int *hidden, float *mean, float *std, int *max_pool_stride);
 #endif
